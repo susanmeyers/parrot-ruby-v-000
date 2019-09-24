56a7b4a4a15021a5726124a9dd3fd6225b867753
Susan Meyers <smmeyers24@gmail.com>
2019-09-24 19:20:21 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,22 +1,2 @@
-def parrot(bird = "Squawk!")
-  puts bird
-
-  # Create method `parrot` that outputs a given phrase and
-  #{phrase}"
-  return bird
-
-
-
-  #define parrot with arg of bird and default arg. of phrase
-   #{phrase}"
-end
-
-
-
-
-
-
-  #return
-
-  # returns the phrase
-#parrot
+# Create method `parrot` that outputs a given phrase and
+# returns the phrase
